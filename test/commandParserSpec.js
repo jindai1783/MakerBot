af3f6aa91fd735f77baf8fb7efc8079acf7e9d83
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-11 12:19:57 +0000

    Updated test to use double.

--- test/commandParserSpec.js
+++ test/commandParserSpec.js
@@ -5,21 +5,42 @@ var CommandParser = require('../app/CommandParser.js');
 describe("Command Parser", function() {
 
   var commandParser;
+  var hash;
 
   beforeEach(function(){
-    commandParser = new CommandParser();
+
+    var OneToOneDouble = function() {
+    };
+
+    OneToOneDouble.prototype.getResponse = function(args) {
+        return "https://github.com/makersacademy/course/wiki/121-and-Challenge-Review-slots"
+    };
+
+    hash = { '!bot 121': OneToOneDouble,
+             '!bot lectures': 'https://github.com/makersacademy/course/wiki/Calendar'
+    }
+
+    commandParser = new CommandParser(hash);
   });
 
-  it("it should return the correct content for the 121 command", function() {
+  it("return the correct content for the 121 command", function() {
     commandParser.parse('!bot 121', function(err, data) {
       expect(data).to.equal('https://github.com/makersacademy/course/wiki/121-and-Challenge-Review-slots');
     });
   });
 
-  it("it should return the correct content for the calendar command",  function() {
+   it("return the correct content when an argument is used", function() {
+    commandParser.parse('!bot 121 henry', function(err,data) {
+      expect(data).to.equal('Available');
+    });
+  });
+
+  it("return the correct content for the calendar command",  function() {
     commandParser.parse('!bot lectures', function(err, data) {
       expect(data).to.equal('https://github.com/makersacademy/course/wiki/Calendar');
     });
   });
+
+
 
 });
