6470d45d6f4efc0ffa4923bcc04f6765123af5e3
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-11 12:21:02 +0000

    Created makerbot which works with the slackbot api

---
+++ app/makerbot.js
@@ -0,0 +1,23 @@
+var commandParser = require('./commandParser');
+var commandEngine = new commandParser();
+var slackbot = require('node-slackbot');
+
+var bot = new slackbot('xoxb-3664957276-x4H5juXjVNiHKheMYsz1VdHa');
+
+bot.use(function(message, cb) {
+  if ('message' == message.type) {
+    console.log(message.user + ' said: ' + message.text);
+  }
+  cb();
+});
+
+bot.use(function(message, cb) {
+  if ("message" === message.type) {
+    commandEngine.parse(message.text, function(err, content) {
+      bot.sendMessage(message.channel, content);
+    });
+  };
+});
+
+bot.connect();
+
