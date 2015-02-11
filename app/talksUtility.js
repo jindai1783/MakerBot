d927ef9414881de580bd47fe8cb68268d2191d7b
Ptolemy <ptolemybarnes@gmail.com>
2015-02-11 18:46:54 +0000

    TalksUtility can tell user about the next event and all of todays events.

--- app/talksUtility.js
+++ app/talksUtility.js
@@ -1,12 +1,48 @@
+var GoogleCalendar = require('public-google-calendar')
 
-var TalksUtility = function() {};
-
-TalksUtility.prototype.nextEvent = function() {
-  return 'Bot Party';
+var TalksUtility = function() {
+  this.api    = new GoogleCalendar({ calendarId: 'iammakerbot@gmail.com' });
 };
 
-TalksUtility.prototype.getResponse = function(args) {
-  return 'The next event will be Bot Party'
+TalksUtility.prototype.getResponse = function(args, callback) {
+  if (args[0] === 'next') {
+    this.getEvents(this.nextEvent);
+  };
+
+};
+
+TalksUtility.prototype.getEvents = function(callback) {
+  this.api.getEvents({}, function(error, events) {
+    callback(events);
+  });
+};
+
+TalksUtility.prototype.nextEvent = function(events) {
+  var nextEventText = events[events.length - 1 ].summary;
+  return "The next event will be " + nextEventText;
+};
+
+TalksUtility.prototype.todaysEvents = function(events, nowTime) {
+  events = events.filter(function(event) {
+    return (event.start.getYear()  == nowTime.getYear() &&
+            event.start.getMonth() == nowTime.getMonth() &&
+            event.start.getDate()  == nowTime.getDate());
+  });
+  
+  events = events.map(function(event) {
+    var minutes = (event.start.getMinutes() < 10 ? "0" + event.start.getMinutes() : event.start.getMinutes())
+
+    return "At " + event.start.getHours() + ":" + minutes +
+    " " + event.summary + " will be giving a talk. ";
+  });
+
+  var outputString = "Here is the agenda for today: "
+
+  for(var i = 0; i < events.length; i ++) {
+    outputString += events[i];
+  }
+
+  return outputString;
 };
 
 module.exports = TalksUtility;
--- test/talksSpec.js
+++ test/talksSpec.js
@@ -1,5 +1,5 @@
-var chai = require('chai');
-var expect = chai.expect;
+var chai         = require('chai');
+var expect       = chai.expect;
 var TalksUtility = require('../app/talksUtility.js')
 
 describe('Talks Utility', function() {
@@ -10,7 +10,35 @@ describe('Talks Utility', function() {
     talksUtility = new TalksUtility();
   });
 
-  it('returns the next event', function() {
-    expect(talksUtility.getResponse(['next'])).to.equal('The next event will be Bot Party');
+  it('returns the next event', function(done) {
+    var EventDouble = function() { this.summary = "Bot Party" }
+    var calEvent    = new EventDouble();
+
+    expect(talksUtility.nextEvent([calEvent])).to.equal('The next event will be Bot Party');
+    done();
   });
+
+  it('returns todays events', function(done) {
+    var steveEventDouble = function() { 
+      this.summary = "Steve Jobs";
+      this.start   = new Date(2015, 1, 11, 12, 30);
+      }
+
+    var billEventDouble = function() { 
+      this.summary = "Bill Gates";
+      this.start   = new Date(2015, 1, 11, 18, 0);
+    }
+
+    var futureEventDouble = function() { 
+      this.summary = "Elon Musk";
+      this.start   = new Date(2016, 1, 11, 18);
+    }
+
+    var steveEvent   = new steveEventDouble();
+    var billEvent    = new billEventDouble();
+    var futureEvent  = new futureEventDouble();
+
+    expect(talksUtility.todaysEvents([steveEvent, billEvent, futureEvent], new Date(2015, 1, 11, 10))).to.equal('Here is the agenda for today: At 12:30 Steve Jobs will be giving a talk. At 18:00 Bill Gates will be giving a talk. ');
+    done();
+  })
 });
