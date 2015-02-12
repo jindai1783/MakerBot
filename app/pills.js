e7d96234b118e20d93cf6a5002e2e89d6f1c987a
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-12 14:15:44 +0000

    re-writing getResponse functions

--- app/pills.js
+++ app/pills.js
@@ -1,54 +1,53 @@
 var _ = require('underscore')._;
 
 function Pill(api){
-  this.api = api;
-  this.apiData = null;
-  this.translatedData = null;
-  this.pillNames = [];
-  this.pillLinks
+ this.api = api;
+ this.apiData = null;
+ this.translatedData = null;
+ this.pillLibrary = {};
 }
 
-Pill.prototype.getResponse = function(name, callback) {
-  var self = this;
-    this.getInfo(function(){
-      self.translate(function(data){
-        callback(data);
-      });
-    });
+
+Pill.prototype.getResponse = function(args, callback) {
+ this.getInfo(function() {
+   this.translate(function(data) {
+     this.getLibrary(function(data){
+       callback(this.decision(args[0]));
+     });
+   });
+ });
 };
 
-Pill.prototype.getInfo = function(callback) {
-  var self = this;
-  this.api. readFileInfo('pills', function(err, data){
-    if(err) {
-      callback(err);
-    }
-    self.apiData = data
-     callback(self.apiData);
-  });
 
+Pill.prototype.decision = function(arg) {
+ if(arg) {
+   this.pillLibrary[arg[0]];
+ } else {
+   this.pillLibrary.keys;
+ }
+};
+
+
+Pill.prototype.getInfo = function(callback) {
+ var self = this;
+ this.api.readFileInfo('pills', function(err, data){
+   if(err) callback(err);
+   self.apiData = data;
+   callback(self.apiData);
+ });
 };
 
 Pill.prototype.translate = function(callback) {
-  var data = JSON.parse(this.apiData);
-  callback(this.translatedData = data);
+ var data = JSON.parse(this.apiData);
+ callback(this.translatedData = data);
 };
 
-Pill.prototype.getNames = function(callback) {
-  var dataSize = this.translatedData.length
-  for(i=0; i<dataSize; i++){
-    this.pillNames.push(this.translatedData[i].name)
-  };
-  callback(this.pillNames)
-};
-
-Pill.prototype.getLinks = function(callback) {
-  var dataSize = this.translatedData.length
-  for(i=0; i<dataSize; i++){
-    this.pillNames.push(this.translatedData[i].html_url)
-  };
-  callback(this.pillNames)
-};
-
+Pill.prototype.getLibrary = function(callback) {
+ var dataSize = this.translatedData.length;
+ for(i = 0; i < dataSize; i++) {
+   this.pillLibrary[this.translatedData[i].name] = this.translatedData[i].html_url;
+ }
+ callback(this.pillLibrary);
+}
 
 module.exports = Pill;
--- test/PillsTest.js
+++ test/PillsTest.js
@@ -1,67 +1,54 @@
-
 var chai = require('chai');
 var expect = chai.expect;
 var MakersGitHub = require('../app/makersGitHub.js');
 var Pill = require('../app/pills.js');
 
-
 describe('pill', function() {
 
-  var makers, pill;
+ var makers, pill;
 
-  before(function(){
-    makers = new MakersGitHub();
-    pill = new Pill(makers);
-  });
+ before(function(){
+   makers = new MakersGitHub();
+   pill = new Pill(makers);
+ });
 
-  it('it should be initialized with an API', function() {
-    expect(pill.api).to.equal(makers);
-  });
+ it('it should be initialized with an API', function() {
+   expect(pill.api).to.equal(makers);
+ });
 
-  
-  it('should receive info from API', function(done) {
-    
-    pill.getInfo(function(data) {
-      expect(data).to.contain('IoPill.md');
-      done(); 
-    });
-     
-  });
+ 
+ it('should receive info from API', function(done) { 
+   pill.getInfo(function(data) {
+     expect(data).to.contain('IoPill.md');
+     done(); 
+   });   
+ });
 
-  it('translate data', function(done) {
+ it('translate data', function(done) {
+   pill.getInfo(function(){
+     pill.translate(function(data){
+     expect(data[0].name).to.contain('IoPill.md');
+     done(); 
+     });
+   });
+ });
 
-    pill.getInfo(function(){
-      pill.translate(function(data){
-      expect(data[0].name).to.contain('IoPill.md');
-      done(); 
-      });
-    });
-  });
+ it('it should sanitize data', function(done) {
+   pill.getResponse(function(){
+     pill.getLibrary(function(data){
+       expect(data['strings.md']).to.contain('https://github.com/makersacademy/course/blob/master/pills/strings.md');
+       done(); 
+     });
+   });
+ });
 
-  it('have a getResponse function', function(done) {
 
-    pill.getResponse(function(data){
-    expect(data[0].name).to.contain('IoPill.md');
-    done(); 
-      });
-    });
+ it('it should make good decision', function(done) {
+   pill.getResponse(function(data){
+     expect(pill.decision(['strings.md']).to.contain('https://github.com/makersacademy/course/blob/master/pills/strings.md'));
+     done();
+   });
+ });
 
-  it('it should sanitize data', function(done) {
 
-    pill.getResponse(function(){
-      pill.getNames(function(data){
-      expect(data[0]).to.contain('IoPill.md')
-    done(); 
-    });
-  });
-
-     it('it should sanitize data', function(done) {
-
-    pill.getResponse(function(){
-      pill.getLinks(function(data){
-      expect(data[0]).to.contain('https://github.com/makersacademy/course/blob/master/pills/IoPill.md')
-    done(); 
-    });
-  });
-});
 });
