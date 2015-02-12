5875dc80fe8f5d59c11e9583181394f672f57fa7
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-12 17:25:56 +0000

    Removed console log

--- test/PillsUtilitySpec.js
+++ test/PillsUtilitySpec.js
@@ -15,21 +15,20 @@ describe('pill', function() {
 
  it('Should return names of pills', function(done){
     pill.getResponse([], function(){
-      console.log('hello2');
       expect(pill.translatedData[0].name).to.contain('IoPill.md');
       done();
-    }); 
+    });
  });
 
  it('Should return names of pills pillLibrary', function(done){
     pill.getResponse([], function(){
      expect(pill.pillLibrary['IoPill.md']).to.contain('https://github.com/makersacademy/course/blob/master/pills/IoPill.md');
      done();
-    }); 
+    });
  });
 
 });
- 
+
 
 
 
