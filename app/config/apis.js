ad348e9d15a9b68709d577486db2558428005b91
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-12 19:50:36 +0000

    Did integration test for challenges

--- app/config/apis.js
+++ app/config/apis.js
@@ -1,10 +1,12 @@
-var One2OneApi    = require('../makersGithub.js');
+var One2OneApi      = require('../makersGithub.js');
 var TalksUtilityApi = require('public-google-calendar');
 var MakersGithub    = require('../makersGithub');
 
 
-var utilitiesApis = { '121': One2OneApi,
-                      'talks' : TalksUtilityApi,
-                      'pills' : MakersGithub }
+var utilitiesApis = { '121'       : One2OneApi,
+                      'talks'     : TalksUtilityApi,
+                      'pills'     : MakersGithub,
+                      'challenge' : MakersGithub
+                    }
 
 module.exports = utilitiesApis;
--- app/config/commands.js
+++ app/config/commands.js
@@ -1,12 +1,14 @@
-var One2One         = require('../one2OneUtility.js');
-var TalksUtility    = require('../talksUtility');
-var WolframUtility  = require('../wolframUtility');
-var PillsUtility    = require('../pillsUtility');
+var One2One           = require('../one2OneUtility.js');
+var TalksUtility      = require('../talksUtility');
+var WolframUtility    = require('../wolframUtility');
+var PillsUtility      = require('../pillsUtility');
+var ChallengeUtility  = require('../challengeUtility');
 
 var commands   = { '121': One2One,
                    'talks' : TalksUtility,
                    'q' : WolframUtility,
-                   'pills' : PillsUtility
+                   'pills' : PillsUtility,
+                   'challenge' : ChallengeUtility
                   }
 
 module.exports = commands;
